f3d88f671eff87be66fec127c0914b3cd61ade5c
alexiovi2000 <iovino.alex@gmail.com>
2016-11-12 14:38:02 +0100

    api

---
+++ api/index.php
@@ -0,0 +1,21 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
+
+require_once '../constants.php';
+
+
+
+echo json_encode(array("pippo"=>"gigi"));
+
+
+/**
+ * /api/login
+ * 
+ * 
+ * username
+ * password
+ */
+
+
+
